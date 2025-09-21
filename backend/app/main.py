c32539fd9ce113f40bd4f1ad296966abc3d6c9b3
Voppor41 <egorselifanov334@yandex.com>
2025-09-21 14:35:02 +0300

    Update main

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,17 +7,4 @@ app = FastAPI()
 async def health():
     return {"status": "Ok"}
 
-@app.get("/ai/quest")
-async def ai_quest():
-
-    user = {
-        "username": "Test user",
-        "level": 3,
-        "mood": "среднее",
-        "habits": "часто скроллит соцсети, пропускает утренние рутины",
-        "goals": "повысить продуктивность"
-    }
-
-    quests = generate_daily_quest(user)
-    return quests
 
